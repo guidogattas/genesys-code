0073332d42eba8fa3bb0c3cc09d94af280d200d5
Guido Gattás <guido.gattas@gmail.com>
2025-03-12 13:45:11 -0300

    [CHANGE] Logo color change

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
-import logo from "../../../../public/images/logo.png";
+import logo from "../../../../public/images/logo2.png";
 
 const Header = () => {
     return (
@@ -8,7 +8,7 @@ const Header = () => {
             <section className='flex flex-col lg:flex-row items-center justify-between px-6 py-4 bg-navBarBG text-mainText font-bold text-[18px]'>
                 {/* Logo Section */}
                 <div className='flex items-center justify-between lg:w-1/2 gap-6'>
-                    <Link to="/" className="block py-4 md:py-8 font-bold text-center transition-all duration-300 transform scale-100 hover:scale-110 hover:opacity-80">
+                    <Link to="/" className="block pulse-heart py-4 md:py-8 font-bold text-center transition-all duration-300 transform scale-100 hover:scale-110 hover:opacity-80">
                         <img src={logo} alt="Logo" className="w-auto h-[120px] md:h-[150px] max-w-[150px]" />
                     </Link>
                     {/* Header Title */}
