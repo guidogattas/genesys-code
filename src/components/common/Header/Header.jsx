c337ae74bf0061d434ca149a556d2119d155f5a3
Guido Gattas <guido.gattas@gmail.com>
2025-03-20 19:32:50 -0300

    [FIX] animacion pulse en Genesys Code

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
             <section className='flex flex-col lg:flex-row items-center justify-between px-6 py-4 bg-navBarBG text-mainText font-bold text-[18px]'>
                 {/* Logo Section */}
                 <div className='flex items-center gap-4 mdbackdrop::mb-4 lg:w-1/2'>
-                    <Link to="/" className="flex items-center gap-2 transition-all duration-300 transform scale-100 pulse-heart">
-                        <img src={logo} alt="Logo" className="w-auto h-[80px] md:h-[100px]" />
+                    <Link to="/" className="flex items-center gap-2 transition-all duration-300 ">
+                        <img src={logo} alt="Logo" className="w-auto h-[80px] md:h-[100px] transform scale-100 pulse-heart" />
                         {/* Header Title */}
                         <h1 className="font-bold font-inter text-[24px]">
                             <span className="text-[#F07C3F]">GENESYS</span> <span className="text-teal-300">CODE</span>
